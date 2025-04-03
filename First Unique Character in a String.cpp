4aa7ad237ed542f4707cfee35209bcbe87d16504
vivek6126 <vivekdodiya4545@gmail.com>
2025-04-03 13:37:46 +0530

    Update First Unique Character in a String.cpp

--- First Unique Character in a String.cpp
+++ First Unique Character in a String.cpp
@@ -3,18 +3,17 @@ public:
       int firstUniqChar(const std::string& s) {
         std::vector<int> freq(26, 0);
 
-        // Count frequency of each character
+     
         for (char ch : s) {
             ++freq[ch - 'a'];
         }
 
-        // Find the first unique character
         for (int i = 0; i < s.size(); ++i) {
             if (freq[s[i] - 'a'] == 1) {
                 return i;
             }
         }
 
-        return -1; // No unique character found
-    }
+        return -1;
+      }
 };
