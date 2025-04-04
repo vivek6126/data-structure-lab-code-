1394b6097327f9646adbc321b868232025ac1ff1
vivek6126 <vivekdodiya4545@gmail.com>
2025-04-04 12:20:10 +0530

    Update Valid Sudoku.cpp

--- Valid Sudoku.cpp
+++ Valid Sudoku.cpp
@@ -1,16 +1,21 @@
+#include <vector>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
-    bool isValidSudoku(const std::vector<std::vector<char>>& board) {
-        std::unordered_set<std::string> seen;
+    bool isValidSudoku(const vector<vector<char>>& board) {
+        unordered_set<string> seen;
 
         for (int i = 0; i < 9; ++i) {
             for (int j = 0; j < 9; ++j) {
                 char num = board[i][j];
                 if (num == '.') continue;
 
-                std::string row_check = "row" + std::to_string(i) + num;
-                std::string col_check = "col" + std::to_string(j) + num;
-                std::string box_check = "box" + std::to_string(i / 3) + std::to_string(j / 3) + num;
+                string row_check = "row" + to_string(i) + num;
+                string col_check = "col" + to_string(j) + num;
+                string box_check = "box" + to_string(i / 3) + to_string(j / 3) + num;
 
                 if (seen.count(row_check) || seen.count(col_check) || seen.count(box_check)) {
                     return false;
